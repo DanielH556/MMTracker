9c2320a4ca7572d881bdf981ada15e43c5abe997
Daniel Eiji Hattori Hossaki <daniel.hossaki@outlook.com>
2023-11-24 16:20:20 -0300

    first commit

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hahaha haha haha sucker</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tracker_repo/databases/items.js
@@ -0,0 +1,137 @@
+var items = [
+  {
+  "Ocarina_of_Time" : [{
+    "id" : "1",
+    "location" : "starts_with",
+    "icon_inactive": "mm_tracker_resources/ocarina_of_time_uns.png",
+    "icon_active": ""
+  }],
+  "Hero_bow" : [{
+    "id" : "2",
+    "location" : "woodfall_temple_miniboss_room",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Fire_Arrow" : [{
+    "id" : "3",
+    "location" : "snowhead_temmple_miniboss_room",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Ice_Arrow" : [{
+    "id" : "4",
+    "location" : "greatbay_temple_miniboss_room",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Light_Arrow" : [{
+    "id" : "5",
+    "location" : "stonetower_temple_light_arrow_room",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bombs" : [{
+    "id" : "6",
+    "location" : [ "bomb_shop", "armos", "beamos", "nejiron" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bombchu" : [{
+    "id" : "7",
+    "location" : "bomb_shop",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Deku_Stick" : [{
+    "id" : "8",
+    "location" : "withered_deku_baba",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Deku_Nut" : [{
+    "id" : "9",
+    "location" : [ "deku_baba", "mini_baba" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Magic_Beans" : [{
+    "id" : "10",
+    "location" : "starts_with",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Powder_Keg" : [{
+    "id" : "11",
+    "location" : "starts_with",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Pictograph_Box" : [{
+    "id" : "12",
+    "location" : "starts_with",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Lens_of_Truth" : [{
+    "id" : "13",
+    "location" : "starts_with",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Hookshot" : [{
+    "id" : "14",
+    "location" : "starts_with",
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Great_Fairy_Sword" : [{
+    "id" : "15",
+    "location" : "starts_with",
+    "requirements" : [ "" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bottle_Kotake" : [{
+    "id" : "16",
+    "location" : "southern_swamp",
+    "requirements" : [ "southern_swamp" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bottle_Goron_Race" : [{
+    "id" : "17",
+    "location" : "mountain_village_path",
+    "requirements" : [ "goron_mask", "goht_remains" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bottle_Alien_Invasion" : [{
+    "id" : "18",
+    "location" : "romani_ranch",
+    "requirements" : [ "powder_keg", "goron_mask" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bottle_Beaver_Race" : [{
+    "id" : "19",
+    "location" : "zora_cape",
+    "requirements" : [ "hookshot" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bottle_Dampe" : [{
+    "id" : "20",
+    "location" : "ikana_graveyard",
+    "requirements" : [ "epona_song", "captain_hat" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }],
+  "Bottle_Madame_Aroma" : [{
+    "id" : "21",
+    "location" : "milk_bar",
+    "requirements" : [ "romani_mask", "special_delivery_letter" ],
+    "icon_inactive": "",
+    "icon_active": ""
+  }]
+}
+]
